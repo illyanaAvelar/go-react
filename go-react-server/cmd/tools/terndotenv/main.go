5822694776a58c68cae2bbd401b0805ff94d8ac1
illyanaAvelar <illyavelar012@gmail.com>
2024-08-07 16:32:02 -0300

    creating first migrations

---
+++ go-react-server/cmd/tools/terndotenv/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"C:/Users/illya/go/bin/tern.exe",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
