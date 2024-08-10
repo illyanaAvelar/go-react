d4dfd8897f4d0404c01ed20dc4ed51a2a041beeb
illyanaAvelar <illyavelar012@gmail.com>
2024-08-10 15:17:33 -0300

    adding room and create-room components

--- react-web/src/app.tsx
+++ react-web/src/app.tsx
@@ -1,3 +1,24 @@
+import { Toaster } from "sonner";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateRoom />,
+  },
+  {
+    path: "/room/:roomId",
+    element: <Room />,
+  },
+]);
+
 export function App() {
-  return <>Hello!</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </>
+  );
 }
